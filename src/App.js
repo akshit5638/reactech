e4d191fdb11eb4807800bc28e14375d8a5bc6752
Akshit Tyagi <akshit4644@gmail.com>
2023-12-25 17:07:18 +0530

    initial file

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Footer from "./components/Footer";
 import "./styles/App.scss"
 import "./styles/header.scss"
 import "./styles/home.scss"
+import "./styles/footer.scss"
 function App() {
   return (
     <Router>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-
       </Routes>
+      <Footer />
     </Router>
   );
 
